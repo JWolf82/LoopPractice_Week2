8541f7b1c85b1640a3b2ced7f1485edd8e7b51b9
JWolf82 <jlwolf82@gmail.com>
2016-04-08 16:41:40 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// for(var i=0; i <= 500; i+=100){
+    
+//     console.log(i);
+    
+// }
+
+// for(var i=1; i<=64; i*=2){
+//     console.log(i);
+// }
+
+// for(var i =1; i<=3; i++){
+//     for(var x = 1; x<=3; x++){
+//         console.log(x)
+//     }
+
+// }
+
+// for(var i = 0; i<=10; i+=2){
+//     console.log(i)
+// }
+
+// for(var i = 3; i<=15; i+=3){
+//     console.log(i)
+// }
+
+// for(var i = 9; i>=0; i--){
+//     console.log(i)
+// }
+
+// for(var i = 0; i<3; i++){
+//     for(var x = 0; x<=3; x++){
+//         console.log(x)
+//     }
+// }
